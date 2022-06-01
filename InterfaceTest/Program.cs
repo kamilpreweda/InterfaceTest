abe171e73d71d89cc427b2ace6fb62ea210045e3
Kamil Preweda <preweda.kamil@o2.pl>
2022-06-01 16:53:39 +0200

    Added TallGuy class

--- InterfaceTest/Program.cs
+++ InterfaceTest/Program.cs
@@ -1,11 +1,21 @@
 ﻿namespace InterfaceTest
 {
+    public class TallGuy
+    {
+        public string Name;
+        public int Height;
 
+        public void TalkAboutYourself()
+        {
+            Console.WriteLine($"Nazywam się {Name}. Mam {Height} cm wzrostu.");
+        }
+    }
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            TallGuy tallGuy = new TallGuy() { Height = 170, Name = "Kamil" };
+            tallGuy.TalkAboutYourself();
         }
     }
 }
