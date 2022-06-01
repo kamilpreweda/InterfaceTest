63e673472408113deff1efe6b9ad301b8c64d0d8
Kamil Preweda <preweda.kamil@o2.pl>
2022-06-01 17:02:01 +0200

    Added IClown interface

--- InterfaceTest/Program.cs
+++ InterfaceTest/Program.cs
@@ -1,14 +1,23 @@
 ﻿namespace InterfaceTest
 {
-    public class TallGuy
+    public class TallGuy : IClown
     {
         public string Name;
         public int Height;
+        public string FunnyThingIHave { get { return "wielkie buty"; } }
 
         public void TalkAboutYourself()
         {
             Console.WriteLine($"Nazywam się {Name}. Mam {Height} cm wzrostu.");
         }
+
+        public void Honk()
+        {
+            Console.WriteLine("Bip, bip!");
+        }
+
+       
+        
     }
     public class Program
     {
@@ -16,6 +25,8 @@
         {
             TallGuy tallGuy = new TallGuy() { Height = 170, Name = "Kamil" };
             tallGuy.TalkAboutYourself();
+            Console.WriteLine($"Ten wysoki gość ma {tallGuy.FunnyThingIHave}.");
+            tallGuy.Honk();
         }
     }
 }
