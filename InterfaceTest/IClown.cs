40401ac04d8994c51db06f774c0a3ad7167d473c
Kamil Preweda <preweda.kamil@o2.pl>
2022-06-03 15:22:10 +0200

    Added static componetsc to IClown Interface

--- InterfaceTest/IClown.cs
+++ InterfaceTest/IClown.cs
@@ -10,5 +10,22 @@ namespace InterfaceTest
     {
         public string FunnyThingIHave { get; }
         void Honk();
+        protected static Random random = new Random();
+
+        private static int carCapacity = 12;
+
+        public static int CarCapacity
+        {
+            get { return carCapacity; }
+            set
+            {
+                if (value > 10) carCapacity = value;
+                else Console.Error.WriteLine($"Uwaga: pojemność autka, {value}, jest za mała");
+            }
+        }
+        public static string ClownCarDescription()
+        {
+            return $"Liczba klaunów w autku: {random.Next(CarCapacity / 2, CarCapacity)}.";
+        }
     }
 }
